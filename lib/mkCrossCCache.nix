a21e87ed3cf0e7f37280edad62f1bfcaaf964b50
JP Hutchins <jphutchins@gmail.com>
2025-11-23 17:16:18 -0800

    fix

--- lib/mkCrossCCache.nix
+++ lib/mkCrossCCache.nix
@@ -9,11 +9,9 @@ pkgs.symlinkJoin {
   paths = [
     pkgs.ccache
     (pkgs.writeShellScriptBin "cross-ccache-setup" ''
-      export CCACHE_DIR="${workspaceRoot}/.ccache"
+      export CCACHE_DIR="$PWD/${workspaceRoot}/.ccache"
       export CCACHE_MAXSIZE="${maxSize}"
       export CCACHE_IGNOREOPTIONS="-specs=* --specs=*"
-      export CMAKE_C_COMPILER_LAUNCHER="ccache"
-      export CMAKE_CXX_COMPILER_LAUNCHER="ccache"
       mkdir -p "$CCACHE_DIR"
     '')
   ];
