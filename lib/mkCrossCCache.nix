e36bce6aec5439e386e171a902cc7efd3ace0553
JP Hutchins <jphutchins@gmail.com>
2025-11-23 17:04:39 -0800

    fix

--- lib/mkCrossCCache.nix
+++ lib/mkCrossCCache.nix
@@ -4,9 +4,15 @@
 , maxSize
 }:
 
-pkgs.writeShellScriptBin "cross-ccache-setup" ''
-  export CCACHE_DIR="${workspaceRoot}/.ccache"
-  export CCACHE_MAXSIZE="${maxSize}"
-  export CCACHE_IGNOREOPTIONS="-specs=* --specs=*"
-  mkdir -p "$CCACHE_DIR"
-''
+pkgs.symlinkJoin {
+  name = "cross-ccache";
+  paths = [
+    pkgs.ccache
+    (pkgs.writeShellScriptBin "cross-ccache-setup" ''
+      export CCACHE_DIR="${workspaceRoot}/.ccache"
+      export CCACHE_MAXSIZE="${maxSize}"
+      export CCACHE_IGNOREOPTIONS="-specs=* --specs=*"
+      mkdir -p "$CCACHE_DIR"
+    '')
+  ];
+}
